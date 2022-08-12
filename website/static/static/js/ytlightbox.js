190e99743ad08c885fab6274b1633dd39b078f40
alileclerc <51137892+alileclerc@users.noreply.github.com>
2022-08-11 18:13:25 -0700

    Revert "Site redesign"
    This reverts commit 7845c4e016c4e4c879c8aaeb5c6923b52fb1cc5e.

--- website/static/static/js/ytlightbox.js
+++ website/static/static/js/ytlightbox.js
@@ -12,22 +12,18 @@ ytlightbox.querySelector(".ytlightbox-close").addEventListener('click', ev => {
   document.getElementById('ytlightboxembed').innerHTML="";
 })
 
-function ytlightboxinit(){
-  document.querySelectorAll('.ytlightbox').forEach(item => {
-    item.addEventListener('click', ev => {
-      ev.preventDefault();
-      target = ev.currentTarget;
-      const urlarr = target.href.split("?v="); 
-      const url = urlarr[1].split("&")[0];
-      let ytiframe = `<iframe src="https://www.youtube.com/embed/${url}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
-      document.getElementById('ytlightboxembed').innerHTML=ytiframe;
-      document.body.style.overflow="hidden";
-      ytlightbox.className = "ytlightbox-overlay on";
-      setTimeout(function() {
-        ytlightbox.className = "ytlightbox-overlay on showing";
-      }, 0.5)
-    })
+document.querySelectorAll('.ytlightbox').forEach(item => {
+  item.addEventListener('click', ev => {
+    ev.preventDefault();
+    target = ev.currentTarget;
+    const urlarr = target.href.split("?v="); 
+    const url = urlarr[1].split("&")[0];
+    let ytiframe = `<iframe src="https://www.youtube.com/embed/${url}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+    document.getElementById('ytlightboxembed').innerHTML=ytiframe;
+    document.body.style.overflow="hidden";
+    ytlightbox.className = "ytlightbox-overlay on";
+    setTimeout(function() {
+      ytlightbox.className = "ytlightbox-overlay on showing";
+    }, 0.5)
   })
-}
-
-ytlightboxinit()
+})
